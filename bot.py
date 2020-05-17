ae43396e9ec5cc073bf0b8c9dba1b21baf4c460e
HiemSword <hiemsword08@pm.me>
2020-05-17 17:47:18 +0200

    Added "!ping" and "!say" commands

--- bot.py
+++ bot.py
@@ -3,43 +3,29 @@ import os
 import discord
 import random
 from dotenv import load_dotenv
+from discord.ext import commands
 
-load_dotenv() # Carichiamo le variabili del file .env
+
+load_dotenv() # Load the .env file
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+client = commands.Bot(command_prefix='!')
+
 
 @client.event
-async def on_ready(): # se il bot stabilisce un collegamento a discord ed è pronto a operare fai:
-    guild = discord.utils.get(client.guilds, name=GUILD) # richiedi id di tutte le guild a cui il bot è collegato
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-
-@client.event
-async def on_message(message): # se il bot rileva un nuovo messaggio fai:
-
-    # Questo if impedisce al bot di rispondersi da solo nel caso il messaggio
-    # che stiamo cercando sia uguale alla risposta del bot
-    if message.author == client.user: 
-        return
-    
-    if message.content == 'Test':
-        response = 'Hello World'
-        await message.channel.send(response)
-    
-    if message.content == 'raise-exception':
-        raise discord.DiscordException
-
-@client.event
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
+async def on_ready():  # When the bot is connected to Discord do:
+    print('Bot is ready')
 
 
-client.run(TOKEN) # avvia bot
+@client.command()
+async def ping(ctx): # !ping command
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')  # Send the latency of the bot
+
+@client.command()
+async def say(ctx, *, arg): # !say command
+    await ctx.send(arg) # Send what the user just said
+
+
+
+client.run(TOKEN) #Start the bot
