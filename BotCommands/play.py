8e03d00b1354e4bd831366f9deb894f0fd9cd51a
Giacomo Persichini <giacomopersichini@gmail.com>
2020-06-15 11:33:30 +0100

    Add example logging of the Play command

--- BotCommands/play.py
+++ BotCommands/play.py
@@ -3,6 +3,10 @@ import discord
 import asyncio
 import json
 from discord.ext import commands
+import logging
+
+logger = logging.getLogger(__name__)
+
 #simport bot
 
 #client = commands.Bot(command_prefix='!') #Command prefix
@@ -27,10 +31,12 @@ class Basic(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-    
+
     # !play command
     @commands.command()
-    async def play(self, ctx, arg): 
+    async def play(self, ctx, arg):
+        logger.debug("Play command called")
+
         if arg == "reset":
             await ctx.send('Ok! Setting my playing status to: `default`')
             await commands.change_presence(activity=discord.Game(name=f'Hello! I am the NDD Bot! version {bot_branch}|{bot_version}'))
